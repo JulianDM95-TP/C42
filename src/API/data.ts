74f55114de02a7b57957babe7aff46e6ae48353c
macarorn <marianacifuenteszu@gmail.com>
2022-03-19 18:37:54 -0500

    usuarios

--- src/API/data.ts
+++ src/API/data.ts
@@ -1,40 +1,15 @@
-import { ICollection, IThing } from "../Interfaces/Interfaces";
+import { IUser } from "../Interfaces/Interfaces";
 
-const SHIRTS_VERANO: IThing[] =
-    [
-        {
-            url: "camisa-1",
-            name: "Camisa 1",
-            description: "Esta camisa es chimba",
-            imgs: [],
-            sizes: ["M", "L", "XL"],
-            colors: ["RED", "GREEN"],
-            price: 1234
-        },
-        {
-            url: "camisa-2",
-            name: "Camisa 2",
-            description: "Esta camisa no es chimba",
-            imgs: [],
-            sizes: ["M", "L", "XL"],
-            colors: ["RED", "GREEN"],
-            price: 1234
-        },
-        {
-            url: "camisa-3",
-            name: "Camisa 3",
-            description: "Esta camisa es muy chimba",
-            imgs: [],
-            sizes: ["M", "L", "XL"],
-            colors: ["RED", "GREEN"],
-            price: 1234
-        }
-    ]
+const usuario1: IUser = {
+    name: "Martin",
+    collections: []
+}
 
-export const COLLECTIONS: ICollection = {
-    url: "coleccion-1",
-    name: "Verano",
-    description: "Las manos en el ano",
-    year: new Date(),
-    things: SHIRTS_VERANO
+const usuario2: IUser = {
+    name: "Jose",
+    collections: []
+}
+const usuario3: IUser = {
+    name: "Maria",
+    collections: []
 }
